da7db64f12087a7bbe22a830b94f7afee2d80f6b
Kavidu M Kulathunga <kavidumalshankulathunga@gmail.com>
2025-10-30 16:27:32 +0530

    update main

--- resources/views/AdminDashboard/master.blade.php
+++ resources/views/AdminDashboard/master.blade.php
@@ -4,8 +4,7 @@
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="Cuba admin is super flexible, powerful, clean & modern responsive bootstrap 5 admin template with unlimited possibilities.">
-  <meta name="keywords" content="admin template, Cuba admin template, dashboard template, flat admin template, responsive admin template, web app">
+  <meta name="description" content="WebCeylon System">
   <meta name="author" content="pixelstrap">
   <meta name="csrf-token" content="{{ csrf_token() }}"><!-- CSRF in HEAD -->
 
